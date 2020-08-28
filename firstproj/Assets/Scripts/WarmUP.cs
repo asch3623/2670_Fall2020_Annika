3ed0ca0ed60b8f1f02d93e3bdcbe1125c6edd1c7
Annika <10848892@my.uvu.edu>
2020-08-27 19:12:17 -0600

    warmup

---
+++ firstproj/Assets/Scripts/WarmUP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarmUP : MonoBehaviour
+{
+
+    public int score = 7;
+    public float time;
+    public bool isRunning;
+    void Start()
+    {
+        if (score == 7)
+        {
+            Debug.Log("your score is 7");
+        }
+    }
+}
