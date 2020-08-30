4b1e9b04564114b85b09577c37b87c9d3d243350
Annika <10848892@my.uvu.edu>
2020-08-29 22:26:11 -0600

    wip

--- firstproj/Assets/Scripts/WarmUP.cs
+++ firstproj/Assets/Scripts/WarmUP.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class WarmUP : MonoBehaviour
 {
 
+enum Direction {North, East, South, West};
+
     public int score = 7;
     public float time;
     public bool isRunning;
+    private Rigidbody rb;
     void Start()
     {
         if (score == 7)
         {
             Debug.Log("your score is 7");
+            rb = GetComponent<Rigidbody>();
         }
     }
 }
