fc8c3bda9b53fbf046acdb8e409ecd858b47cbc0
Annika <10848892@my.uvu.edu>
2020-09-04 22:09:45 -0600

    Interactions

---
+++ firstproj/Assets/Scripts/Interact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interact : MonoBehaviour
+{
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactEvent;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (Input.GetKeyDown(interactKey))
+            {
+                interactEvent.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+}
