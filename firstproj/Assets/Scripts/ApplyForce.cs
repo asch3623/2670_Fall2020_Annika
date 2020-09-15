87ee006ad86e08182db96feba480bf3a2190256c
Annika <10848892@my.uvu.edu>
2020-09-14 23:47:24 -0600

    Instancer

---
+++ firstproj/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force = 30f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        var forceDirection = new Vector3(force, 0,0);
+        rb.AddRelativeForce(forceDirection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
