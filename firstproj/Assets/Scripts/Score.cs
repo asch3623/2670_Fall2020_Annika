d9fbc2142c45510cffe16f902c3ba761edcf7d3b
Annika <10848892@my.uvu.edu>
2020-09-27 22:43:09 -0600

    Score

---
+++ firstproj/Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Score : MonoBehaviour
+{
+    
+    private Text scoreText;
+   public IntData score;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+    void Update()
+    {
+        scoreText.text = "Lives: " + score.value.ToString();
+    }
+}
