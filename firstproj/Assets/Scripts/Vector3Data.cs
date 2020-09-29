4e92b6a4390b94deeebee1b6aeded570943ada17
Annika <10848892@my.uvu.edu>
2020-09-28 22:06:10 -0600

    Vector3data

---
+++ firstproj/Assets/Scripts/Vector3Data.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataTypes
+{
+   [CreateAssetMenu]
+   public class Vector3Data : ScriptableObject
+   {
+      public Vector3 value;
+
+      public void ChangePosition(Transform transformObj)
+      {
+         transformObj.position = value;
+      }
+
+      public void ChangeVector3Data(Vector3Data vector3DataObj)
+      {
+         vector3DataObj.value = value;
+      }
+
+      public void ChangeValueFromTransform(Transform transformObj)
+      {
+         value = transformObj.position;
+      }
+   }
+}
