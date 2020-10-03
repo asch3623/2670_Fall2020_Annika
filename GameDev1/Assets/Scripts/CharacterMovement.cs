70be1babf113017404e69a7d4b1b0e711053b47c
Annika <10848892@my.uvu.edu>
2020-10-02 19:37:32 -0600

    Dart Movement

--- GameDev1/Assets/Scripts/CharacterMovement.cs
+++ GameDev1/Assets/Scripts/CharacterMovement.cs
@@ -1,27 +1,60 @@
-﻿using System;
+﻿using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CharacterMovement : MonoBehaviour
 {
   private Rigidbody rb;
-  public float moveSpeed = 20f;
+  public float moveSpeed = 10f;
+  public bool isDarting;
+  private int seconds = 3, coolDownSeconds = 10;
+  public BoolData coolDown;
 
   private void Start()
   {
     rb = GetComponent<Rigidbody>();
-    Vector3 rotateRB;
-
+    coolDown.value = false;
   }
 
-  private void Update()
+  private void FixedUpdate()
   
   {
     rb.transform.rotation = Quaternion.Euler(0,0,0);
     
     float moveH = Input.GetAxis("Horizontal");
     float moveV = Input.GetAxis("Vertical");
-    Vector3 movement = new Vector3(moveH, 0f, moveV)* Time.deltaTime * moveSpeed;
+    Vector3 movement = new Vector3(moveH, 0f, moveV)* Time.fixedDeltaTime * moveSpeed;
     rb.MovePosition(transform.position + movement);
+    
+   
+
+  }
+
+  private void Update()
+  {
+     if (Input.GetKeyUp(KeyCode.LeftShift) && isDarting == false && coolDown.value == false)
+        {
+          isDarting = true;
+          moveSpeed = 50f;
+          StartCoroutine(Dart());
+        }
+    
+        if (coolDown.value == true)
+        {
+          StartCoroutine(reloadDart());
+        }
+  }
+
+  IEnumerator Dart()
+  {
+    yield return new WaitForSeconds(seconds);
+    moveSpeed = 20f;
+    isDarting = false;
+    coolDown.value = true;
+  }
+  IEnumerator reloadDart()
+  {
+    yield return new WaitForSeconds(coolDownSeconds);
+    coolDown.value = false;
   }
 }
---
+++ GameDev1/Assets/Scripts/FillImageBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class FillImageBehavior : MonoBehaviour
+{
+  private Image im;
+  public BoolData coolDown;
+  private float fillValue = 0f;
+  private float targetValue = 1f;
+  private float fillSpeed = .1f;
+    void Start()
+    {
+
+        im = GetComponent<Image>();
+        im.fillAmount = fillValue;
+    }
+    
+    void Update()
+    {
+        while (coolDown.value == true && fillValue <= 1f)
+        {
+            fillValue = Mathf.MoveTowards(fillValue, targetValue, Time.deltaTime * fillSpeed);
+            im.fillAmount = fillValue;
+        }
+    }
+}
