f9d8205d1c154bb4b40a7cac9bcf9c209fc2b25e
Annika <10848892@my.uvu.edu>
2020-10-02 18:25:01 -0600

    walk and run

---
+++ GameDev1/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterMovement : MonoBehaviour
+{
+  private Rigidbody rb;
+  public float moveSpeed = 20f;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+    Vector3 rotateRB;
+
+  }
+
+  private void Update()
+  
+  {
+    rb.transform.rotation = Quaternion.Euler(0,0,0);
+    
+    float moveH = Input.GetAxis("Horizontal");
+    float moveV = Input.GetAxis("Vertical");
+    Vector3 movement = new Vector3(moveH, 0f, moveV)* Time.deltaTime * moveSpeed;
+    rb.MovePosition(transform.position + movement);
+  }
+}
