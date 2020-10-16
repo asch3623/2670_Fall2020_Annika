b825118b230e00fe6fe2ca9ec6070d2618199802
Annika <10848892@my.uvu.edu>
2020-10-15 21:47:51 -0600

    bulletBehavior

---
+++ firstproj/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class BulletBehaviour : MonoBehaviour
+{
+//script made in dgm 2221 class
+    private Rigidbody rBody;
+    public float bulletForce;
+    public float lifeTime;
+
+    private IEnumerator Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        
+        rBody.AddRelativeForce(Vector3.forward * bulletForce);
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+    
+}
