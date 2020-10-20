b0bf3f5abb4d58e7ed75543c8f6b0fd6d8e925ab
Annika <10848892@my.uvu.edu>
2020-10-19 21:12:57 -0600

    StringLists

---
+++ firstproj/Assets/Scripts/StringListData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class StringListData : ScriptableObject
+{
+  
+  public List<string> stringList;
+  private string returnValue;
+
+  private int i;
+
+  private void OnEnable()
+  {
+    i = 0;
+  }
+
+  public void GetNextString()
+  {
+    returnValue = stringList[i];
+    i = (i+1) % stringList.Count;
+  }
+
+  public void SetTextUiToValue(Text obj)
+  {
+    
+  }
+
+}
