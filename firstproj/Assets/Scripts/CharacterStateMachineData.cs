89fa68b1279e39198f9cc987f664a1713fe5ec9b
Annika <10848892@my.uvu.edu>
2020-10-21 16:39:08 -0600

    State Machine Intro

---
+++ firstproj/Assets/Scripts/CharacterStateMachineData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class CharacterStateMachineData : ScriptableObject
+{
+    public enum characterStates{
+        StandardWalk,
+        WallCrawl,
+     KnockBack
+    }
+    
+    public characterStates value;
+
+    public UnityEvent onEnableEvent;
+
+    public void OnEnable()
+    {
+        onEnableEvent.Invoke();
+    }
+
+    public void StandardWalk()
+    {
+        value = characterStates.StandardWalk;
+    }
+    public void KnockBack()
+    {
+        value = characterStates.KnockBack;
+    }
+    
+    public void WallClimb()
+    {
+        value = characterStates.WallCrawl;
+    }
+
+}
---
+++ firstproj/Assets/Scripts/CharacterWithStates.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CharacterWithStates : MonoBehaviour
+{​​
+
+private CharacterController controller;
+public CharacterStateMachineData characterStates;
+private Vector3 movement;
+public float moveSpeed = 3f;
+public float gravity = -9.81f;
+private void Start()
+{​​
+    controller = GetComponent<CharacterController>();
+}​​
+
+private void Update()
+{ var newInput = Input.GetAxis("Vertical");
+    switch (characterStates.value)
+    {
+        case CharacterStateMachineData.characterStates.StandardWalk:
+            movement.Set(newInput,gravity,0);
+            break;
+        case CharacterStateMachineData.characterStates.WallCrawl:
+            movement.Set(0,newInput,0);
+            break;
+        case CharacterStateMachineData.characterStates.KnockBack:
+            print("knock back");
+            break;
+    }
+
+    var newMovement = movement * (moveSpeed * Time.deltaTime);
+    controller.Move(newMovement);
+}​​
+
+}​​
