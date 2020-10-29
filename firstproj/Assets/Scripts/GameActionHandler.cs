e9e19e818b8e191631ff7a766c657362668c633f
Annika <10848892@my.uvu.edu>
2020-10-28 22:26:55 -0600

    Unity Actions

---
+++ firstproj/Assets/Scripts/GameActionHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    
+    public GameAction gameAction;
+    public UnityEvent handlerEvent;
+    private void Start()
+    {
+        gameAction.action += ActionHandler;
+    }
+
+    private void ActionHandler()
+    {
+        handlerEvent.Invoke();
+    }
+}
